dba89e256132741d920c5446c4f01ed1d0931362
st64539 <petrcerv999@gmail.com>
2024-09-30 18:32:44 +0200

    Refaktorizována třída ComplexNumber

---
+++ NNPTPZ1/ComplexNumber.cs
@@ -0,0 +1,79 @@
+﻿using System;
+
+namespace NNPTPZ1.Mathematics
+{
+    public class ComplexNumber
+    {
+        public double RealPart { get; set; }
+        public float ImaginaryPart { get; set; }
+
+        public readonly static ComplexNumber Zero = new ComplexNumber()
+        {
+            RealPart = 0,
+            ImaginaryPart = 0
+        };
+
+        public double GetAbsoluteValue()
+        {
+            return Math.Sqrt(RealPart * RealPart + ImaginaryPart * ImaginaryPart);
+        }
+
+        public double GetAngleInRadians()
+        {
+            return Math.Atan(ImaginaryPart / RealPart);
+        }
+
+        public ComplexNumber Add(ComplexNumber addend)
+        {
+            return new ComplexNumber()
+            {
+                RealPart = RealPart + addend.RealPart,
+                ImaginaryPart = ImaginaryPart + addend.ImaginaryPart
+            };
+        }
+
+        public ComplexNumber Subtract(ComplexNumber subtrahend)
+        {
+            return new ComplexNumber()
+            {
+                RealPart = RealPart - subtrahend.RealPart,
+                ImaginaryPart = ImaginaryPart - subtrahend.ImaginaryPart
+            };
+        }
+
+        public ComplexNumber Multiply(ComplexNumber multiplicand)
+        {
+            return new ComplexNumber()
+            {
+                RealPart = RealPart * multiplicand.RealPart - ImaginaryPart * multiplicand.ImaginaryPart,
+                ImaginaryPart = (float)(RealPart * multiplicand.ImaginaryPart + ImaginaryPart * multiplicand.RealPart)
+            };
+        }
+
+        public ComplexNumber Divide(ComplexNumber divisor)
+        {
+            var numerator = Multiply(new ComplexNumber() { RealPart = divisor.RealPart, ImaginaryPart = -divisor.ImaginaryPart });
+            var denominator = divisor.RealPart * divisor.RealPart + divisor.ImaginaryPart * divisor.ImaginaryPart;
+
+            return new ComplexNumber()
+            {
+                RealPart = numerator.RealPart / denominator,
+                ImaginaryPart = (float)(numerator.ImaginaryPart / denominator)
+            };
+        }
+
+        public override string ToString()
+        {
+            return $"({RealPart} + {ImaginaryPart}i)";
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj is ComplexNumber x)
+            {
+                return x.RealPart == RealPart && x.ImaginaryPart == ImaginaryPart;
+            }
+            return base.Equals(obj);
+        }
+    }
+}
