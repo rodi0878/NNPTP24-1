d8ee5271f6d94f668b1f26dfb5ff9725ba6dedd9
Roman Divis <roman.divis@upce.cz>
2024-09-30 10:18:19 +0200

    update

---
+++ NNPTPZ1/ComplexNumber.cs
@@ -0,0 +1,87 @@
+﻿using System;
+
+namespace NNPTPZ1.Mathematics
+{
+    public class ComplexNumber
+    {
+        public double RealPart { get; set; }
+        public double ImaginaryPart { get; set; }
+
+        public readonly static ComplexNumber Zero = new ComplexNumber()
+        {
+            RealPart = 0,
+            ImaginaryPart = 0
+        };
+
+        public override bool Equals(object obj)
+        {
+            if (obj is ComplexNumber)
+            {
+                ComplexNumber x = obj as ComplexNumber;
+                // Math.Abs(x.RealPart - RealPart) < 0.00001
+                return x.RealPart == RealPart && x.ImaginaryPart == ImaginaryPart;
+            }
+            return base.Equals(obj);
+        }
+
+        public ComplexNumber Add(ComplexNumber b)
+        {
+            ComplexNumber a = this;
+            return new ComplexNumber()
+            {
+                RealPart = a.RealPart + b.RealPart,
+                ImaginaryPart = a.ImaginaryPart + b.ImaginaryPart
+            };
+        }
+        public ComplexNumber Subtract(ComplexNumber b)
+        {
+            ComplexNumber a = this;
+            return new ComplexNumber()
+            {
+                RealPart = a.RealPart - b.RealPart,
+                ImaginaryPart = a.ImaginaryPart - b.ImaginaryPart
+            };
+        }
+
+        public ComplexNumber Multiply(ComplexNumber b)
+        {
+            ComplexNumber a = this;
+            // aRe*bRe + aRe*bIm*i + aIm*bRe*i + aIm*bIm*i*i
+            return new ComplexNumber()
+            {
+                RealPart = a.RealPart * b.RealPart - a.ImaginaryPart * b.ImaginaryPart,
+                ImaginaryPart = a.RealPart * b.ImaginaryPart + a.ImaginaryPart * b.RealPart
+            };
+        }
+        public ComplexNumber Divide(ComplexNumber b)
+        {
+            // (aRe + aIm*i) / (bRe + bIm*i)
+            // ((aRe + aIm*i) * (bRe - bIm*i)) / ((bRe + bIm*i) * (bRe - bIm*i))
+            //  bRe*bRe - bIm*bIm*i*i
+            ComplexNumber tmp = this.Multiply(new ComplexNumber() { RealPart = b.RealPart, ImaginaryPart = -b.ImaginaryPart });
+            double tmp2 = b.RealPart * b.RealPart + b.ImaginaryPart * b.ImaginaryPart;
+
+            return new ComplexNumber()
+            {
+                RealPart = tmp.RealPart / tmp2,
+                ImaginaryPart = tmp.ImaginaryPart / tmp2
+            };
+        }
+
+        public double GetAbs()
+        {
+            return Math.Sqrt(RealPart * RealPart + ImaginaryPart * ImaginaryPart);
+        }
+
+        public double GetAngle()
+        {
+            return Math.Atan(ImaginaryPart / RealPart);
+        }
+
+        public override string ToString()
+        {
+            return $"({RealPart} + {ImaginaryPart}i)";
+        }
+
+    }
+}
