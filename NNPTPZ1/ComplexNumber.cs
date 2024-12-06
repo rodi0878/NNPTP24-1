a47d93c235fbf406cde33477029c571d6b2b049d
verysvadym <veresivadim3@gmail.com>
2024-12-06 14:24:22 +0100

    Refactoring

---
+++ NNPTPZ1/ComplexNumber.cs
@@ -0,0 +1,84 @@
+﻿using System;
+
+namespace NNPTPZ1.Mathematics
+{
+    public class ComplexNumber
+    {
+        public double Re { get; set; }
+        public double Im { get; set; }
+
+        public static readonly ComplexNumber Zero = new ComplexNumber
+        {
+            Re = 0,
+            Im = 0
+        };
+
+        public override bool Equals(object obj)
+        {
+            if (obj is ComplexNumber other)
+            {
+                return other.Re == Re && other.Im == Im;
+            }
+            return false;
+        }
+
+        public ComplexNumber Add(ComplexNumber other)
+        {
+            return new ComplexNumber
+            {
+                Re = this.Re + other.Re,
+                Im = this.Im + other.Im
+            };
+        }
+
+        public ComplexNumber Subtract(ComplexNumber other)
+        {
+            return new ComplexNumber
+            {
+                Re = this.Re - other.Re,
+                Im = this.Im - other.Im
+            };
+        }
+
+        public ComplexNumber Multiply(ComplexNumber other)
+        {
+            return new ComplexNumber
+            {
+                Re = this.Re * other.Re - this.Im * other.Im,
+                Im = this.Re * other.Im + this.Im * other.Re
+            };
+        }
+
+        public ComplexNumber Divide(ComplexNumber other)
+        {
+            double denominator = other.Re * other.Re + other.Im * other.Im;
+            var numerator = this.Multiply(new ComplexNumber { Re = other.Re, Im = -other.Im });
+
+            return new ComplexNumber
+            {
+                Re = numerator.Re / denominator,
+                Im = numerator.Im / denominator
+            };
+        }
+
+        public double GetAbsoluteValue()
+        {
+            return Math.Sqrt(Re * Re + Im * Im);
+        }
+
+        public double GetAngleInRadians()
+        {
+            return Math.Atan(Im / Re);
+        }
+
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
+
+        public override string ToString()
+        {
+            return $"({Re} + {Im}i)";
+        }
+    }
+}
---
+++ NNPTPZ1Tests/ComplexNumberTests.cs
@@ -0,0 +1,95 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NNPTPZ1.Mathematics;
+using System;
+
+namespace NNPTPZ1.Mathematics.Tests
+{
+    [TestClass]
+    public class ComplexNumberTests
+    {
+        [TestMethod]
+        public void AddTest()
+        {
+            ComplexNumber a = new ComplexNumber
+            {
+                Re = 10,
+                Im = 20
+            };
+            ComplexNumber b = new ComplexNumber
+            {
+                Re = 1,
+                Im = 2
+            };
+
+            ComplexNumber actual = a.Add(b);
+            ComplexNumber expected = new ComplexNumber
+            {
+                Re = 11,
+                Im = 22
+            };
+
+            Assert.AreEqual(expected, actual);
+
+            string expectedString = "(10 + 20i)";
+            string actualString = a.ToString();
+            Assert.AreEqual(expectedString, actualString);
+
+            expectedString = "(1 + 2i)";
+            actualString = b.ToString();
+            Assert.AreEqual(expectedString, actualString);
+
+            a = new ComplexNumber
+            {
+                Re = 1,
+                Im = -1
+            };
+            b = new ComplexNumber
+            {
+                Re = 0,
+                Im = 0
+            };
+            expected = new ComplexNumber
+            {
+                Re = 1,
+                Im = -1
+            };
+            actual = a.Add(b);
+            Assert.AreEqual(expected, actual);
+
+            expectedString = "(1 + -1i)";
+            actualString = a.ToString();
+            Assert.AreEqual(expectedString, actualString);
+
+            expectedString = "(0 + 0i)";
+            actualString = b.ToString();
+            Assert.AreEqual(expectedString, actualString);
+        }
+
+        [TestMethod]
+        public void AddTestPolynomial()
+        {
+            Polynomial polynomial = new Polynomial();
+            polynomial.Coefficients.Add(new ComplexNumber { Re = 1, Im = 0 });
+            polynomial.Coefficients.Add(new ComplexNumber { Re = 0, Im = 0 });
+            polynomial.Coefficients.Add(new ComplexNumber { Re = 1, Im = 0 });
+
+            ComplexNumber result = polynomial.Evaluate(new ComplexNumber { Re = 0, Im = 0 });
+            ComplexNumber expected = new ComplexNumber { Re = 1, Im = 0 };
+            Assert.AreEqual(expected, result);
+
+            result = polynomial.Evaluate(new ComplexNumber { Re = 1, Im = 0 });
+            expected = new ComplexNumber { Re = 2, Im = 0 };
+            Assert.AreEqual(expected, result);
+
+            result = polynomial.Evaluate(new ComplexNumber { Re = 2, Im = 0 });
+            expected = new ComplexNumber { Re = 5.0000000000, Im = 0 };
+            Assert.AreEqual(expected, result);
+
+            string expectedPolynomialString = "(1 + 0i) + (0 + 0i)x + (1 + 0i)xx";
+            string actualPolynomialString = polynomial.ToString();
+            Assert.AreEqual(expectedPolynomialString, actualPolynomialString);
+        }
+    }
+}
+
+
